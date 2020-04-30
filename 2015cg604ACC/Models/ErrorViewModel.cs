bfbd5f70bf98d624f40e2ee817faa871b59441c1
leo.contreras <luis.contreras1@catolica.edu.sv>
2020-04-30 12:03:49 -0600

    primer commit

---
+++ 2015cg604ACC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace _2015cg604ACC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
